777bce017609de30a50a5eea6af81e5ba50844ac
Cédric <nifix@users.noreply.github.com>
2019-07-27 18:40:22 +0200

    Add files via upload

---
+++ AceCaisse/AceCaisse/Main.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace AceCaisse
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.groupBoxInput = new System.Windows.Forms.GroupBox();
+            this.launchParse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.importTotal = new System.Windows.Forms.Label();
+            this.importProgressBar = new System.Windows.Forms.ProgressBar();
+            this.chooseImportFile = new System.Windows.Forms.Button();
+            this.importFileTextbox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listRemises = new System.Windows.Forms.ListView();
+            this.export = new System.Windows.Forms.Button();
+            this.pickImportFile = new System.Windows.Forms.OpenFileDialog();
+            this.groupBoxInput.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxInput
+            // 
+            this.groupBoxInput.Controls.Add(this.launchParse);
+            this.groupBoxInput.Controls.Add(this.label1);
+            this.groupBoxInput.Controls.Add(this.labelStatus);
+            this.groupBoxInput.Controls.Add(this.importTotal);
+            this.groupBoxInput.Controls.Add(this.importProgressBar);
+            this.groupBoxInput.Controls.Add(this.chooseImportFile);
+            this.groupBoxInput.Controls.Add(this.importFileTextbox);
+            this.groupBoxInput.Location = new System.Drawing.Point(14, 11);
+            this.groupBoxInput.Name = "groupBoxInput";
+            this.groupBoxInput.Size = new System.Drawing.Size(613, 119);
+            this.groupBoxInput.TabIndex = 0;
+            this.groupBoxInput.TabStop = false;
+            this.groupBoxInput.Text = "Fichier d\'entrée";
+            // 
+            // launchParse
+            // 
+            this.launchParse.Location = new System.Drawing.Point(532, 17);
+            this.launchParse.Margin = new System.Windows.Forms.Padding(0);
+            this.launchParse.Name = "launchParse";
+            this.launchParse.Size = new System.Drawing.Size(75, 23);
+            this.launchParse.TabIndex = 5;
+            this.launchParse.Text = "Lancer !";
+            this.launchParse.UseVisualStyleBackColor = true;
+            this.launchParse.Click += new System.EventHandler(this.LaunchParse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic Semilight", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(402, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ce fichier doit être un export du compte 512 directement par la fenêtre de révisi" +
+    "on.";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.BackColor = System.Drawing.Color.Transparent;
+            this.labelStatus.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.ForeColor = System.Drawing.Color.Black;
+            this.labelStatus.Location = new System.Drawing.Point(5, 84);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(411, 23);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "En attente de la selection du fichier ...";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // importTotal
+            // 
+            this.importTotal.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.importTotal.Location = new System.Drawing.Point(423, 84);
+            this.importTotal.Name = "importTotal";
+            this.importTotal.Size = new System.Drawing.Size(184, 23);
+            this.importTotal.TabIndex = 3;
+            this.importTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // importProgressBar
+            // 
+            this.importProgressBar.Location = new System.Drawing.Point(6, 58);
+            this.importProgressBar.MarqueeAnimationSpeed = 0;
+            this.importProgressBar.Name = "importProgressBar";
+            this.importProgressBar.Size = new System.Drawing.Size(601, 23);
+            this.importProgressBar.Step = 1;
+            this.importProgressBar.TabIndex = 2;
+            // 
+            // chooseImportFile
+            // 
+            this.chooseImportFile.Location = new System.Drawing.Point(437, 17);
+            this.chooseImportFile.Name = "chooseImportFile";
+            this.chooseImportFile.Size = new System.Drawing.Size(92, 23);
+            this.chooseImportFile.TabIndex = 1;
+            this.chooseImportFile.Text = "Selectionner ...";
+            this.chooseImportFile.UseVisualStyleBackColor = true;
+            this.chooseImportFile.Click += new System.EventHandler(this.ChooseImportFile_Click);
+            // 
+            // importFileTextbox
+            // 
+            this.importFileTextbox.BackColor = System.Drawing.SystemColors.Control;
+            this.importFileTextbox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.importFileTextbox.ForeColor = System.Drawing.Color.Black;
+            this.importFileTextbox.Location = new System.Drawing.Point(6, 17);
+            this.importFileTextbox.Name = "importFileTextbox";
+            this.importFileTextbox.ReadOnly = true;
+            this.importFileTextbox.Size = new System.Drawing.Size(425, 20);
+            this.importFileTextbox.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listRemises);
+            this.panel1.Location = new System.Drawing.Point(12, 136);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(615, 341);
+            this.panel1.TabIndex = 1;
+            // 
+            // listRemises
+            // 
+            this.listRemises.Location = new System.Drawing.Point(2, 0);
+            this.listRemises.Name = "listRemises";
+            this.listRemises.Size = new System.Drawing.Size(613, 338);
+            this.listRemises.TabIndex = 0;
+            this.listRemises.UseCompatibleStateImageBehavior = false;
+            // 
+            // export
+            // 
+            this.export.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.export.Location = new System.Drawing.Point(505, 483);
+            this.export.Name = "export";
+            this.export.Size = new System.Drawing.Size(122, 23);
+            this.export.TabIndex = 2;
+            this.export.Text = "Exporter vers Excel";
+            this.export.UseVisualStyleBackColor = true;
+            this.export.Click += new System.EventHandler(this.Export_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 513);
+            this.Controls.Add(this.export);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBoxInput);
+            this.Font = new System.Drawing.Font("Malgun Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Espace ACE - Caisse v1.0";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_FormClosed);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.groupBoxInput.ResumeLayout(false);
+            this.groupBoxInput.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxInput;
+        private System.Windows.Forms.Button chooseImportFile;
+        private System.Windows.Forms.TextBox importFileTextbox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listRemises;
+        private System.Windows.Forms.ProgressBar importProgressBar;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label importTotal;
+        private System.Windows.Forms.Button export;
+        private System.Windows.Forms.Button launchParse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog pickImportFile;
+    }
+}
+
---
+++ AceCaisse/AceCaisse/Main.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Globalization;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace AceCaisse
+{
+    public partial class Main : Form
+    {
+
+        private String _defaultFileTestingPurpose  = @"C:\\Users\\Nifix\\Documents\\sample.xlsx";
+        private String _defaultExportPath = @"C:\Users\Nifix\Documents\";
+        private double rowCount;
+        private double colCount;
+        private double montantTotal = 0;
+        BackgroundWorker bgw;
+        delegate void AddItemListViewCallback(string curDate, string curOperation, string curAmount);
+        delegate void UpdateProgressBarCallback(int maxValue);
+
+        
+
+        public Main()
+        {
+            InitializeComponent();
+            CreateBackGroundWorker();
+            this.export.Enabled = false;
+            this.launchParse.Enabled = false;
+            this.pickImportFile.Title = "Choisissez un fichier à importer";
+            this.pickImportFile.DefaultExt = "xlsx";
+            this.pickImportFile.Filter = "Fichiers Excel (*.xlsx)|*.xlsx|Fichiers Excel (Anciens) (*.xls)|*.xls|(Tous les fichiers (*.*)|*.*";
+            this.pickImportFile.FilterIndex = 1;
+            this.pickImportFile.CheckFileExists = true;
+            this.pickImportFile.CheckPathExists = true;
+            this.pickImportFile.Multiselect = false;
+            this.pickImportFile.InitialDirectory = @"C:\";
+        }
+
+        private void CreateBackGroundWorker()
+        {
+            bgw = new BackgroundWorker();
+            bgw.WorkerReportsProgress = true;
+            bgw.WorkerSupportsCancellation = true;
+
+            bgw.DoWork += new DoWorkEventHandler(this.ParseExcelFile);
+            bgw.ProgressChanged += new ProgressChangedEventHandler(this.bgw_ProgressChanged);
+            bgw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(this.bgw_RunWorkCompleted);
+        }
+
+        /* 
+         * Function used when the user selects a file through
+         * the Select File Dialog.
+         * 
+         * TODO : Add the SelectDialog function, check if the file is correct or not.
+         * 
+        */
+        private void ChooseImportFile_Click(object sender, EventArgs e)
+        {
+           try
+            {
+                DialogResult dr = this.pickImportFile.ShowDialog();
+                if (dr == DialogResult.OK)
+                {
+                    this._defaultFileTestingPurpose = this.pickImportFile.FileName;
+                    this._defaultExportPath = Path.GetDirectoryName(this._defaultFileTestingPurpose);
+                    this.importFileTextbox.Text = this._defaultFileTestingPurpose;
+                    this.labelStatus.Text = "Fichier prêt pour l'importation.";
+                    this.launchParse.Enabled = true;
+                } else
+                {
+                    MessageBox.Show("Erreur dans la selection du fichier.", "Snif :(", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Erreur dans la selection du fichier.", "Snif :(", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw ex;
+            }
+        }
+
+        /* 
+         * ProgressChanged function from Background Worker
+         * Used when we load a file in the ListView.
+         * 
+        */
+        private void bgw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.importProgressBar.Value = e.ProgressPercentage;
+            this.labelStatus.Text = e.ProgressPercentage.ToString() + " opérations traitées...";
+            this.importTotal.Text = this.montantTotal.ToString() + "€";
+        }
+
+        /* 
+         * RunWorkerCompleted function from BackgroundWorker
+         * Used when we the import is done or cancelled.
+         * 
+        */
+        private void bgw_RunWorkCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                labelStatus.Text = "Annulation ..";
+            } else if (e.Error != null)
+            {
+                labelStatus.Text = "Erreur : " + e.Error.Message;
+            } else
+            {
+                labelStatus.Text = "Réussite !";
+                this.importProgressBar.Value = this.importProgressBar.Minimum;
+                this.chooseImportFile.Enabled = true;
+                this.launchParse.Enabled = true;
+                this.export.Enabled = true;
+            }
+        }
+
+        /* 
+         * DoWork function from BackgroundWorker
+         * It loads the data from the Excel file directly to the ListView.
+         * 
+        */
+        private void ParseExcelFile(object sender, DoWorkEventArgs e)
+        {
+            this.montantTotal = 0;
+            BackgroundWorker worker = sender as BackgroundWorker;
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(this._defaultFileTestingPurpose);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            this.rowCount = xlRange.Rows.Count;
+            this.colCount = xlRange.Columns.Count;
+            this.SetProgressBar((int)this.rowCount);
+
+            String strDate, curOperation, curAmount;
+            DateTime curDate;
+
+            for (int i = 2; i <= rowCount; i++)
+            {
+ 
+                curDate = DateTime.FromOADate(xlRange[i, 1].Value2);
+                strDate = curDate.ToString("dd/MM/yyyy");
+                curOperation = xlRange[i, 4].Value2.ToString();
+                curAmount = xlRange[i, 5].Value2.ToString();
+
+                if (curOperation.StartsWith("REMISE "))
+                {
+                    montantTotal += Convert.ToDouble(curAmount);
+                    this.AddItemListView(strDate, curOperation, curAmount);
+                }
+
+                worker.ReportProgress(i);
+                System.Threading.Thread.Sleep(2);
+            }
+
+
+            Marshal.ReleaseComObject(xlRange);
+            xlRange = null;
+            Marshal.ReleaseComObject(xlWorksheet);
+            xlWorksheet = null;
+            xlWorkbook.Close(false, Type.Missing, Type.Missing);
+            Marshal.ReleaseComObject(xlWorkbook);
+            xlWorkbook = null;
+            xlApp.Application.UserControl = true;
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+            xlApp = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        /* 
+         * Function used to add data in the listview with delegate.
+         * 
+        */
+        private void AddItemListView(string curDate, string curOperation, string curAmount)
+        {
+            if (this.listRemises.InvokeRequired)
+            {
+                AddItemListViewCallback d = new AddItemListViewCallback(AddItemListView);
+                this.Invoke(d, new object[] { curDate, curOperation, curAmount });
+            }
+            else
+            {
+                ListViewItem newRow = new ListViewItem();
+                newRow.Text = curDate;
+                newRow.SubItems.Add(curOperation);
+                newRow.SubItems.Add(curAmount);
+                listRemises.Items.Add(newRow);
+            }
+        }
+
+        /* 
+         * Function used to update the progressbar with delegate.
+         * 
+        */
+        private void SetProgressBar(int maxValue)
+        {
+            if (this.importProgressBar.InvokeRequired)
+            {
+                this.importProgressBar.Invoke(new UpdateProgressBarCallback(SetProgressBar), new object[] { maxValue });
+            }
+            else
+            {
+                this.importProgressBar.Maximum = maxValue;
+                this.importProgressBar.Minimum = 2;
+                this.importProgressBar.Step = 1;
+            }
+        }
+
+        /* 
+         * Function used to add data in the listview with delegate.
+         * 
+        */
+        private void Main_Load(object sender, EventArgs e)
+        {
+            listRemises.View = View.Details;
+            ColumnHeader headerDate, headerOperation, headerMontant;
+
+            headerDate = new ColumnHeader();
+            headerOperation = new ColumnHeader();
+            headerMontant = new ColumnHeader();
+
+            headerDate.Text = "Date";
+            headerDate.Width = 100;
+            headerOperation.Text = "Operation";
+            headerOperation.Width = 300;
+            headerMontant.Text = "Montant";
+            headerMontant.Width = 100;
+
+            listRemises.Columns.Add(headerDate);
+            listRemises.Columns.Add(headerOperation);
+            listRemises.Columns.Add(headerMontant);
+
+            listRemises.GridLines = true;
+
+        }
+
+        /* 
+         * On Closed
+         * 
+        */
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void Export_Click(object sender, EventArgs e)
+        {
+
+            int i = 2;
+            this.export.Enabled = false;
+            this.export.Text = "Export en cours ...";
+
+            Excel.Application exportapp = new Excel.Application();
+            Excel.Workbook exportwb = exportapp.Workbooks.Add(1);
+            Excel.Worksheet exportws = (Excel.Worksheet)exportwb.Worksheets[1];
+            exportws.Columns.ColumnWidth = 18;
+            Excel.Range columnDate = exportws.Range["A1"].EntireColumn;
+            Excel.Range columnPieceCompte = exportws.Range["B1:C1"].EntireColumn;
+            Excel.Range columnLibelle = exportws.Range["D1"].EntireColumn;
+            Excel.Range columnAmount = exportws.Range["E1:F1"].EntireColumn;
+
+            columnDate.NumberFormat = "DD/MM/YYYY";
+            columnDate.ColumnWidth = 10;
+            columnPieceCompte.ColumnWidth = 10;
+            columnLibelle.ColumnWidth = 19;
+            columnAmount.ColumnWidth = 10;
+
+
+
+            DateTime exDate;
+            String strexDate, exLibelle;
+            Double exAmount, exRemisecb, exRemiseCheque, exRemiseTr, exRemiseEsp;
+
+            exportws.Cells[1, 1] = "Date";
+            exportws.Cells[1, 2] = "Pièce";
+            exportws.Cells[1, 3] = "Compte";
+            exportws.Cells[1, 4] = "Libellé";
+            exportws.Cells[1, 5] = "Débit";
+            exportws.Cells[1, 6] = "Crédit";
+
+            exRemisecb = 58000001;
+            exRemiseCheque = 58000002;
+            exRemiseTr = 58000003;
+            exRemiseEsp = 58000004;
+
+
+
+            foreach (ListViewItem lvi in listRemises.Items)
+            {
+
+                exDate = Convert.ToDateTime(lvi.SubItems[0].Text);
+                exLibelle = lvi.SubItems[1].Text;
+                exAmount = Convert.ToDouble(lvi.SubItems[2].Text);
+
+                // Génération Ligne 1 avec les 58xxxx
+                strexDate = exDate.ToString("MM/dd/yyyy");
+                exportws.Cells[i, 1] = strexDate;
+
+
+                // Génération dynamique selon le type de remise
+
+                if (exLibelle.StartsWith("REMISE CB"))
+                {
+                    exportws.Cells[i, 3] = exRemisecb;
+                } else if (exLibelle.StartsWith("REMISE CHEQUE"))
+                {
+                    exportws.Cells[i, 3] = exRemiseCheque;
+                } else if (exLibelle.StartsWith("REMISE ESPECE"))
+                {
+                    exportws.Cells[i, 3] = exRemiseEsp;
+                } else if (exLibelle.StartsWith("REMISE TR"))
+                {
+                    exportws.Cells[i, 3] = exRemiseTr;
+                }
+
+                exportws.Cells[i, 4] = exLibelle;
+                exportws.Cells[i, 5] = exAmount;
+
+                // Génération Ligne 2 avec le 530
+                exportws.Cells[i + 1, 1] = strexDate;
+
+                exportws.Cells[i + 1, 3] = 53000000;
+                exportws.Cells[i + 1, 4] = exLibelle;
+                exportws.Cells[i + 1, 6] = exAmount;
+
+                i += 2;
+            }
+
+            //exportwb.SaveAs(this._defaultExportPath + "export-data.xlsx");
+            DateTime timenow = DateTime.Now;
+            
+            exportwb.SaveAs(this._defaultExportPath + "\\Export-caisse-" + timenow.ToString("HH_mm_ss") + ".xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault);
+
+
+
+            Marshal.ReleaseComObject(exportws);
+            exportws = null;
+            exportwb.Close(false, Type.Missing, Type.Missing);
+            Marshal.ReleaseComObject(exportwb);
+            exportwb = null;
+            exportapp.Application.UserControl = true;
+            exportapp.Quit();
+            Marshal.ReleaseComObject(exportapp);
+            exportapp = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            this.export.Enabled = true;
+            this.export.Text = "Exporter vers Excel";
+
+            DialogResult openYesNo = MessageBox.Show("Export réalisé !\r\n\r\nFichier disponible : \r\n" + this._defaultExportPath + "\\Export-caisse-" + timenow.ToString(
+                "HH_mm_ss") + ".xlsx\r\n\r\n" +
+                "Voulez-vous ouvrir le fichier maintenant ?", "Export to Excel", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            switch (openYesNo)
+            {
+                case DialogResult.Yes:
+                    System.Diagnostics.Process.Start(this._defaultExportPath + "\\Export-caisse-" + timenow.ToString("HH_mm_ss") + ".xlsx");
+                    break;
+                case DialogResult.No:
+                    break;
+            }
+        }
+
+        private void LaunchParse_Click(object sender, EventArgs e)
+        {
+            listRemises.Items.Clear();
+            if (bgw.IsBusy != true)
+            {
+                this.chooseImportFile.Enabled = false;
+                this.launchParse.Enabled = false;
+                this.export.Enabled = false;
+                bgw.RunWorkerAsync();
+            }
+        }
+    }
+}
